af0951135f4752c7720097d6a29a2072934232de
Chris <filmee24@gmail.com>
2022-06-17 12:17:14 +0200

    Add Helper Methods To LeaBuilder

--- Builder/LeaBuilder.cs
+++ Builder/LeaBuilder.cs
@@ -6,16 +6,31 @@ namespace LeaVM.Builder
     {
         public List<Instruction> instructions = new();
 
-        public void Emit(Instruction instruction)
-        {
-            instructions.Add(instruction);
-        }
-
         public byte[] AsBytes()
         {
             var result = new List<byte>();
             instructions.ForEach(_ => result.AddRange(_.AsBytes()));
             return result.ToArray();
+        }
+
+        public void Emit(Instruction instruction)
+        {
+            instructions.Add(instruction);
+        }
+
+        public void Emit(OpCodes opcode)
+        {
+            Emit(new Instruction(opcode));
+        }
+
+        public void Emit(OpCodes opcode, Operand operand)
+        {
+            Emit(new Instruction(opcode, operand));
+        }
+
+        public void Emit(OpCodes opcode, Operand operand1, Operand operand2)
+        {
+            Emit(new Instruction(opcode, operand1, operand2));
         }
     }
 }
